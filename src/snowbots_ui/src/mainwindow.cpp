521acebe5a4e5031a72ba83e6d07e6dc3570354f
Roozki <zawadzkirowan@gmail.com>
2023-02-25 16:06:59 -0800

    updated GUI

--- src/snowbots_ui/src/mainwindow.cpp
+++ src/snowbots_ui/src/mainwindow.cpp
@@ -46,9 +46,17 @@ MainWindow::~MainWindow() {
 }
 
 void MainWindow::twist_values() {
-    ui->left_lcd->display(twist_message_left.linear.x);
-    ui->right_lcd->display(twist_message_right.linear.x);
-    ui->angular_lcd->display(twist_message_controller.angular.z);
-    ui->linear_lcd->display(twist_message_controller.linear.x);
-    ros_f->twist_subscriber();
+    ui->mode_lcd->setText("Hello World"); // replace hellow world with message for mode
+    ui->nuc_temp_lcd->display(twist_message_right.linear.x); // message req for updating nuc temp
+    ui->rover_temp_lcd->display(twist_message_controller.angular.z); // message req for updating rover temp
+    ui->elec_box_temp_lcd->display(twist_message_controller.linear.x); // message req for updating elec box temp
+    ros_f->twist_subscriber(); // message req for updating elec box temp
 }
+// TODOS: 
+// Current Mode - status - done
+// end effector position feedback (hopefully)
+// end effector force feedback as % of grip
+// nuc temperature
+// rover ambient temp
+// elec box temp
+// stored arm poses (button)
